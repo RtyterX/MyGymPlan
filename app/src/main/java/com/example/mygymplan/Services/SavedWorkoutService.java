5f81190b8a4dab73367a2b7e46285216c2db905f
RtyterX <ricardothiagofirmino@hotmail.com>
2025-10-29 01:28:01 -0300

    Saved Workout Service
    Signed-off-by: RtyterX <ricardothiagofirmino@hotmail.com>

---
+++ app/src/main/java/com/example/mygymplan/Services/SavedWorkoutService.java
@@ -0,0 +1,58 @@
+package com.example.mygymplan.Services;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.example.mygymplan.Database.AppDatabase;
+import com.example.mygymplan.Database.SavedWorkoutDao;
+import com.example.mygymplan.Entitys.SavedWorkout;
+
+public class SavedWorkoutService extends AppCompatActivity {
+
+        public void saveMyWorkout(Context context, SavedWorkout savedWorkout) {
+
+            AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, "workouts").build();
+            SavedWorkoutDao dao = db.savedWorkoutDao();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dao.insertSavedWorkout(savedWorkout);
+                }
+            }).start();
+
+            db.close();
+        }
+
+        public void updateSavedWorkout(Context context, SavedWorkout savedWorkout) {
+
+            AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, "workouts").build();
+            SavedWorkoutDao dao = db.savedWorkoutDao();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dao.updateSavedWorkout(savedWorkout);
+                }
+            }).start();
+
+            db.close();
+        }
+
+        public void deleteSavedWorkout(Context context, SavedWorkout savedWorkout) {
+
+            AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, "workouts").build();
+            SavedWorkoutDao dao = db.savedWorkoutDao();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dao.deleteSavedWorkout(savedWorkout);
+                }
+            }).start();
+
+            db.close();
+        }
+}
